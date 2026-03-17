d69ace4ef20f9ffabe50387dc3ff2fc8507275a2
David <197518179+CheerfulBear22@users.noreply.github.com>
2026-03-17 17:10:41 +0000

    Added the AddFilm, RemoveFilm, FindFilm, and BinarySearch functions to Cinema.cs

--- CinemaProject/CinemaProject/Cinema.cs
+++ CinemaProject/CinemaProject/Cinema.cs
@@ -25,7 +25,59 @@ namespace CinemaProject
         public void AddFilm(string film)
         {
             //Adding a film
-            if (Films.Count == 0) Films.Add(film);
+            if (Films.Count == 0)
+            {
+                Films.Add(film);
+                return;
+            }
+            for (int i = 0; i < Films.Count; i++)
+            {
+                if (String.Compare(film, Films[i]) < 1)
+                {
+                    Films.Insert(i, film);
+                    return;
+                }
+            }
+
+            Films.Add(film);
+        }
+
+        public void RemoveFilm(string film)
+        {
+            //Remove a film from the list using the FindFilm function
+            int index = FindFilm(film);
+
+            if (index == -1) return;
+
+            Films.RemoveAt(index);
+        }
+
+        static int FindFilm(string film)
+        {
+            return _BinarySearch(Films, film, 0, Films.Count);
+        }
+
+        static int _BinarySearch(List<string> data, string value, int left, int right)
+        {
+            if (left > right)
+            {
+                return -1;
+            }
+
+            int mid = (left + right) / 2;
+
+            if (data[mid].CompareTo(value) < 0)
+            {
+                return _BinarySearch(data, value, mid + 1, right);
+            }
+            else if (data[mid].CompareTo(value) > 0)
+            {
+                return _BinarySearch(data, value, left, mid - 1);
+            }
+            else
+            {
+                return mid;
+            }
         }
     }
 }
