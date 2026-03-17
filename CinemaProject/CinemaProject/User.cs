0fd564815a00edc4102d0adfe4ef5cf1acf3ce84
sward225 <sward225@DL5139>
2026-03-17 12:32:48 +0000

    sammy

--- CinemaProject/CinemaProject/User.cs
+++ CinemaProject/CinemaProject/User.cs
@@ -17,23 +17,25 @@ namespace CinemaProject
 
         public bool GetIsManager(bool status)
         {
-            IsManager = status
+            IsManager = status;
+            return status;
         }
 
         public string GetUserName(string user)
         {
             UserName = user;
+            return user;
         }
 
         //Functions
 
         public void ChangeIsManager(bool newStatus)
         {
-            if (newStatus == 1)
+            if (newStatus == true)
             {
                 IsManager = true;
             }
-            else if (newStatus == 0)
+            else if (newStatus == false)
             {
                 IsManager = false;
             }
