c7c670a10a3e40ed0d6a98b35dff4d72c7156718
agibbons25 <agibbons25@DL5137>
2026-03-17 12:57:36 +0000

    edited Main to include the methods

--- CinemaProject/CinemaProject/Program.cs
+++ CinemaProject/CinemaProject/Program.cs
@@ -10,7 +10,38 @@ namespace CinemaProject
     {
         static void Main(string[] args)
         {
+            User CurrentUser = new User();
+            // needs a menu system for logging in
+        }
 
+        public void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("---- WELCOME TO CINEMA MANAGEMENT SYSTEM ----");
+            Console.WriteLine();
+            Console.WriteLine("ENTER YOUR OPTIONS");
+        }
+
+        public bool Login()
+        {
+            // handles the login and returns true if the user
+            // manages to sign in with correct credentials
+            // within 'x' number of tries
+        }
+
+        public void AddUser(User newUser)
+        {
+            // probably adds the passed user to a list of users
+            // or saves them to a file
+            // or loads them to a file
+            // for David to do
+        }
+
+        public void Save()
+        {
+            // saves the user credentials to the file if it is a new user
+            // or just not do anything if the user is already saved
+            // this means that Main will not call it if therer are no users to add
         }
     }
 }
